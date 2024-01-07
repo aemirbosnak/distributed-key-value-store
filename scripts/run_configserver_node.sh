9a7ad57e374bf30d7bf3419978d4a20c7bddae3f
Emir Boşnak <emir.bosnak@ug.bilkent.edu.tr>
2024-01-08 00:49:31 +0300

    Delete scripts/run_configserver_node.sh

--- scripts/run_configserver_node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NODE_ID=$1
-
-go run ./configserver --node_id "$NODE_ID" --port "300$NODE_ID" --raft_addr "localhost:1300$NODE_ID" \
-    &> "logs/configserver_$NODE_ID.log" &
-
-echo "configserver $NODE_ID is live (pid $!)"
-
